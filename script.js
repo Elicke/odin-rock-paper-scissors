75153c17a50317fc3356b394228a20f4e6c6798e
Kyle Manning <mkylee2@gmail.com>
2024-08-09 00:43:24 -0700

    Add computer choice function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    switch (num) {
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
+    }
+}
