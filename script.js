64a6f1e0f93539f3d769a21f202222b69ca99f49
Kyle Manning <mkylee2@gmail.com>
2024-08-09 01:03:14 -0700

    Add human choice function

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 function getComputerChoice() {
-    let num = Math.floor(Math.random() * 3);
-    switch (num) {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
         case 0:
             return "rock"
         case 1:
             return "paper"
         case 2:
             return "scissors"
+    }
+}
+
+function getHumanChoice() {
+    let choice;
+    let message = "Rock, paper, or scissors?";
+    while (true) {
+        choice = prompt(message);
+        choice = choice.toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            return choice;
+        } else {
+            message = "That's not one of the choices! Rock, paper, or scissors?";
+        }
     }
 }
