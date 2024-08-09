9dee43afdc0fc3c342cc76c5c67fc616d70462ca
Kyle Manning <mkylee2@gmail.com>
2024-08-09 01:47:02 -0700

    Add play round function and change choice strings from lowercase to capitalized

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let computerScore = 0,
     humanScore = 0;
 
+function playRound(computerChoice, humanChoice) {
+    if ((computerChoice === "Rock" && humanChoice === "Scissors")
+        || (computerChoice === "Paper" && humanChoice === "Rock")
+        || (computerChoice === "Scissors" && humanChoice === "Paper")) {
+            ++computerScore;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+    } else if ((humanChoice === "Rock" && computerChoice === "Scissors")
+        || (humanChoice === "Paper" && computerChoice === "Rock")
+        || (humanChoice === "Scissors" && computerChoice === "Paper")) {
+            ++humanScore;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+    } else {
+        console.log(`${computerChoice} and ${humanChoice} -- it's a tie!`);
+    }
+}
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch (choice) {
         case 0:
-            return "rock"
+            return "Rock"
         case 1:
-            return "paper"
+            return "Paper"
         case 2:
-            return "scissors"
+            return "Scissors"
     }
 }
 
@@ -18,8 +34,9 @@ function getHumanChoice() {
     let message = "Rock, paper, or scissors?";
     while (true) {
         choice = prompt(message);
-        choice = choice.toLowerCase();
-        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        // Convert to capitalized form
+        choice = choice.charAt(0).toUpperCase() + choice.toLowerCase().slice(1);
+        if (choice === "Rock" || choice === "Paper" || choice === "Scissors") {
             return choice;
         } else {
             message = "That's not one of the choices! Rock, paper, or scissors?";
