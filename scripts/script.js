f9b944b83ed6bbbbf466294dae3d4e015225d266
Kyle Manning <mkylee2@gmail.com>
2024-08-15 00:35:45 -0700

    Add function for updating imgs in versus

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 const humanUI = document.querySelector("#ui__human");
+const humanChoiceImg = document.querySelector(".ui__choice--human > img");
+const computerChoiceImg = document.querySelector(".ui__choice--computer > img");
 
 humanUI.addEventListener("click", (e) => {
     let target = e.target;
     let parent = target.parentElement;
     let classes = parent.classList;
-    let choice = "";
+    let choice;
 
     if (classes.contains("ui__rock")) {
         choice = "Rock";
@@ -14,42 +16,41 @@ humanUI.addEventListener("click", (e) => {
         choice = "Scissors";
     }
 
-    console.log(choice);
+    if (choice !== null) {
+        playRound(getComputerChoice(), choice);
+    }
 });
 
-// playGame();
-
-function playGame() {
-    let computerScore = 0,
-        humanScore = 0,
-        roundCount = 1;
-
-    function playRound(computerChoice, humanChoice) {
-        if ((computerChoice === "Rock" && humanChoice === "Scissors")
-            || (computerChoice === "Paper" && humanChoice === "Rock")
-            || (computerChoice === "Scissors" && humanChoice === "Paper")) {
-                ++computerScore;
-                ++roundCount;
-                console.log(`You lose this round! ${computerChoice} beats ${humanChoice}!`);
-        } else if ((humanChoice === "Rock" && computerChoice === "Scissors")
-            || (humanChoice === "Paper" && computerChoice === "Rock")
-            || (humanChoice === "Scissors" && computerChoice === "Paper")) {
-                ++humanScore;
-                ++roundCount;
-                console.log(`You win this round! ${humanChoice} beats ${computerChoice}!`);
-        } else {
-            console.log(`${computerChoice} and ${humanChoice} -- it's a tie!`);
-        }
-    }
-
-    while (roundCount <= 5) {
-        playRound(getComputerChoice(), getHumanChoice());
-    }
-
-    console.log((computerScore > humanScore)
-        ? `The computer wins with a score of ${computerScore}-${humanScore}!`
-        : `You won with a score of ${humanScore}-${computerScore}!`);
+function playRound(computerChoice, humanChoice) {
+    updateVersus(computerChoice, humanChoice);
+    // if ((computerChoice === "Rock" && humanChoice === "Scissors")
+    //     || (computerChoice === "Paper" && humanChoice === "Rock")
+    //     || (computerChoice === "Scissors" && humanChoice === "Paper")) {
+    //         ++computerScore;
+    //         console.log(`You lose this round! ${computerChoice} beats ${humanChoice}!`);
+    // } else if ((humanChoice === "Rock" && computerChoice === "Scissors")
+    //     || (humanChoice === "Paper" && computerChoice === "Rock")
+    //     || (humanChoice === "Scissors" && computerChoice === "Paper")) {
+    //         ++humanScore;
+    //         console.log(`You win this round! ${humanChoice} beats ${computerChoice}!`);
+    // } else {
+    //     console.log(`${computerChoice} and ${humanChoice} -- it's a tie!`);
+    // }
 }
+
+// function playGame() {
+//     let computerScore = 0,
+//         humanScore = 0,
+//         roundCount = 1;
+
+//     while (roundCount <= 5) {
+//         playRound(getComputerChoice(), getHumanChoice());
+//     }
+
+//     console.log((computerScore > humanScore)
+//         ? `The computer wins with a score of ${computerScore}-${humanScore}!`
+//         : `You won with a score of ${humanScore}-${computerScore}!`);
+// }
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -58,22 +59,31 @@ function getComputerChoice() {
             return "Rock"
         case 1:
             return "Paper"
-        case 2:
+        default:
             return "Scissors"
     }
 }
 
-function getHumanChoice() {
-    let choice;
-    let message = "Rock, paper, or scissors?";
-    while (true) {
-        choice = prompt(message);
-        // Convert to capitalized form
-        choice = choice.charAt(0).toUpperCase() + choice.toLowerCase().slice(1);
-        if (choice === "Rock" || choice === "Paper" || choice === "Scissors") {
-            return choice;
-        } else {
-            message = "That's not one of the choices! Rock, paper, or scissors?";
-        }
+function updateVersus(computerChoice, humanChoice) {
+    switch (humanChoice) {
+        case "Rock":
+            humanChoiceImg.setAttribute("src", "./images/rock-human.svg");
+            break;
+        case "Paper":
+            humanChoiceImg.setAttribute("src", "./images/paper-human.svg");
+            break;
+        default:
+            humanChoiceImg.setAttribute("src", "./images/scissors-human.svg");
+    }
+
+    switch (computerChoice) {
+        case "Rock":
+            computerChoiceImg.setAttribute("src", "./images/rock-computer.svg");
+            break;
+        case "Paper":
+            computerChoiceImg.setAttribute("src", "./images/paper-computer.svg");
+            break;
+        default:
+            computerChoiceImg.setAttribute("src", "./images/scissors-computer.svg");
     }
 }
